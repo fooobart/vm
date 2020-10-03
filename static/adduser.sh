bbf0152af4ce21135194aafc2c231bf8a8a505d9
fooobart <33230839+fooobart@users.noreply.github.com>
2020-10-04 00:21:22 +0200

    Removed popup window for nextcloud user

--- static/adduser.sh
+++ static/adduser.sh
@@ -16,15 +16,15 @@ debug_mode
 LINUX_USER_NAME=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`LINUX_USER_NAME`].Value' --output text)
 LINUX_USER_PASS=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`LINUX_USER_PASS`].Value' --output text)
 
-if [[ $UNIXUSER != "ncadmin" ]]
-then
-msg_box "Current user with sudo permissions is: $UNIXUSER.
-This script will set up everything with that user.
-If the field after ':' is blank you are probably running as a pure root user.
-It's possible to install with root, but there will be minor errors.
+# if [[ $UNIXUSER != "ncadmin" ]]
+# then
+# msg_box "Current user with sudo permissions is: $UNIXUSER.
+# This script will set up everything with that user.
+# If the field after ':' is blank you are probably running as a pure root user.
+# It's possible to install with root, but there will be minor errors.
 
-Please create a user with sudo permissions if you want an optimal installation.
-The preferred user is 'ncadmin'."
+# Please create a user with sudo permissions if you want an optimal installation.
+# The preferred user is 'ncadmin'."
     # if ! yesno_box_yes "Do you want to create a new user?"
     # then
         # print_text_in_color "$ICyan" "Not adding another user..."
@@ -42,4 +42,4 @@ The preferred user is 'ncadmin'."
         # done
         sudo -u "$LINUX_USER_NAME" sudo bash "$1"
     # fi
-fi
+# fi
