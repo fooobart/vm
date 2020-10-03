6cd646d2bb2543b055804517de4cf5492f005955
fooobart <33230839+fooobart@users.noreply.github.com>
2020-10-03 21:35:42 +0200

    Update nextcloud_install_production.sh
    Added script for updating the IP address of the EC2 machine on Amazon Route 53

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -5,6 +5,32 @@
 # Prefer IPv4 for apt
 echo 'Acquire::ForceIPv4 "true";' >> /etc/apt/apt.conf.d/99force-ipv4
 
+# FFT: Install AWS CLI toolchain
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+sudo rm -f ./awscliv2.zip
+sudo rm -rf ./aws
+
+# FFT: Add Amazon AWS Route53 update script for EC2 startup
+cat << ROUTE53_UPDATE_IP > "/var/lib/cloud/scripts/per-boot/update_route53_address.sh"
+#!/bin/bash
+
+# Extract information about the Instance
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id/)
+AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4/)
+
+# Extract tags associated with instance
+ZONE_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_ZONE`].Value' --output text)
+NAME_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_NAME`].Value' --output text)
+
+# Update Route 53 Record Set based on the Name tag to the current Public IP address of the Instance
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_TAG --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$NAME_TAG'","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$MY_IP'"}]}}]}'
+ROUTE53_UPDATE_IP
+
+chmod +x /var/lib/cloud/scripts/per-boot/update_route53_address.sh
+
 # FFT: Install my favourite essential tools on the VM
 apt install aptitude emacs links
 
