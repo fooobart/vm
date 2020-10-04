650549bfb7bcde03930c3bf0fc8c2b947c99868e
fooobart <33230839+fooobart@users.noreply.github.com>
2020-10-04 23:19:50 +0200

    Only need to set one defined locale which comes from the AWS EC2 tag

--- static/locales.sh
+++ static/locales.sh
@@ -6,31 +6,13 @@
 true
 SCRIPT_NAME="Locales"
 # shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+. ../lib.sh
 
 # Must be root
 root_check
 
 # Set locales
-print_text_in_color "$ICyan" "Setting locales..."
-if [ "$KEYBOARD_LAYOUT" = "us" ]
-then
-    print_text_in_color "$ICyan" "US locales are already set."
-elif [ "$KEYBOARD_LAYOUT" = "se" ]
-then
-    sudo locale-gen "sv_SE.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
-elif [ "$KEYBOARD_LAYOUT" = "de" ]
-then
-    sudo locale-gen "de_DE.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
-elif [ "$KEYBOARD_LAYOUT" = "us" ]
-then
-    sudo locale-gen "en_US.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
-elif [ "$KEYBOARD_LAYOUT" = "fr" ]
-then
-    sudo locale-gen "fr_FR.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
-elif [ "$KEYBOARD_LAYOUT" = "ch" ]
-then
-    sudo locale-gen "de_CH.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
-fi
+sudo locale-gen "de_DE.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
+print_text_in_color "$ICyan" "Setting locales to de_DE"
 
 # TODO: "localectl list-x11-keymap-layouts" and pair with "cat /etc/locale.gen | grep UTF-8"
