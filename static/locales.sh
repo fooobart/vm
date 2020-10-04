31fbcc742bf89313c7feb4be7e4ff973aa7ef9dd
fooobart <33230839+fooobart@users.noreply.github.com>
2020-10-04 23:24:12 +0200

    Read locale from AWS EC2 tag

--- static/locales.sh
+++ static/locales.sh
@@ -19,8 +19,12 @@ aws configure set AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY_ID
 aws configure set AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
 aws configure set default.region $AWS_DEFAULT_REGION
 
+AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id/)
+LOCALE=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`LOCALE`].Value' --output text)
+
 # Set locales
-sudo locale-gen "de_DE.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
+sudo locale-gen "$LOCALE.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
 print_text_in_color "$ICyan" "Setting locales to de_DE"
 
 # TODO: "localectl list-x11-keymap-layouts" and pair with "cat /etc/locale.gen | grep UTF-8"
