ea7568b45339a738731b7a50c0dd5d43158d341a
fooobart <33230839+fooobart@users.noreply.github.com>
2020-10-04 23:21:57 +0200

    Added AWS configuration

--- static/locales.sh
+++ static/locales.sh
@@ -11,6 +11,14 @@ SCRIPT_NAME="Locales"
 # Must be root
 root_check
 
+# FFT: Added configuration of AWS toolchain
+AWS_ACCESS_KEY_ID=$(cat ~/.AWS_ACCESS_KEY_ID)
+AWS_SECRET_ACCESS_KEY=$(cat ~/.AWS_SECRET_ACCESS_KEY)
+AWS_DEFAULT_REGION=$(cat ~/.AWS_DEFAULT_REGION)
+aws configure set AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY_ID
+aws configure set AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
+
 # Set locales
 sudo locale-gen "de_DE.UTF-8" && sudo dpkg-reconfigure --frontend=noninteractive locales
 print_text_in_color "$ICyan" "Setting locales to de_DE"
