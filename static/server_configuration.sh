f40f6563234f9e8152ce04b42c5e8cd398aff8cd
fooobart <33230839+fooobart@users.noreply.github.com>
2020-10-05 10:24:42 +0200

    Removed unnessessary checks for internet connection

--- static/server_configuration.sh
+++ static/server_configuration.sh
@@ -4,24 +4,6 @@
 
 . lib.sh
 
-# If we have internet, then use the latest variables from the lib remote file
-if printf "Testing internet connection..." && ping github.com -c 2
-then
-# shellcheck disable=2034,2059
-true
-SCRIPT_NAME="Server configuration"
-# shellcheck source=lib.sh
-# Use local lib file in case there is no internet connection
-elif [ -f /var/scripts/lib.sh ]
-then
-# shellcheck disable=2034,2059
-true
-else
-    printf "You don't seem to have a working internet connection, and /var/scripts/lib.sh is missing so you can't run this script."
-    printf "Please report this to https://github.com/nextcloud/vm/issues/"
-    exit 1
-fi
-
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
 # 0 = OFF
