8f179b9620601165002f13c878fa1d024f1fba9b
fooobart <33230839+fooobart@users.noreply.github.com>
2020-10-05 10:24:16 +0200

    Switched to local lib.sh

--- static/static_ip.sh
+++ static/static_ip.sh
@@ -9,26 +9,7 @@ print_text_in_color() {
 	printf "%b%s%b\n" "$1" "$2" "$Color_Off"
 }
 
-# Use local lib file in case there is no internet connection
-if [ -f /var/scripts/lib.sh ]
-then
-# shellcheck disable=2034,2059
-true
-SCRIPT_NAME="Static IP"
-# shellcheck source=lib.sh
-source /var/scripts/lib.sh
- # If we have internet, then use the latest variables from the lib remote file
-elif printf "Testing internet connection..." && ping github.com -c 2
-then
-# shellcheck disable=2034,2059
-true
-# shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
-else
-    printf "You don't seem to have a working internet connection, and /var/scripts/lib.sh is missing so you can't run this script."
-    printf "Please report this to https://github.com/nextcloud/vm/issues/"
-    exit 1
-fi
+. lib.sh
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
